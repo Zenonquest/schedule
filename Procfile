7f6eb5bcd4388bd26aca212a7c3004b89da1be47
Zenon <Zenonquest@gmail.com>
2016-04-28 13:02:16 -0600

    second small update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn schedule2.wsgi
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- schedule2/settings.py
+++ schedule2/settings.py
@@ -77,13 +77,8 @@ WSGI_APPLICATION = 'schedule2.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
 
