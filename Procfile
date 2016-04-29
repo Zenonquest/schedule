38fb2eb5b0edd400d927494ddf015834f1e16527
Zenon <Zenonquest@gmail.com>
2016-04-28 23:03:02 -0600

    changed database_url

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn schedule2.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:$PORT
--- schedule2/settings.py
+++ schedule2/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,7 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # from djangogirls
+import dj_database_url
 # DATABASES['default'] = dj_database_url.config()
+
+# DATABASES = {'default': dj_database_url.config(default='postgres://foo:bar@localhost:5432/db')}
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
