db1bfb1acf9b9d8b5eece0b75525d046fefd56eb
Zenon <Zenonquest@gmail.com>
2016-04-28 23:24:02 -0600

    changes staticfiles_dir to asssets

--- schedule2/settings.py
+++ schedule2/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
