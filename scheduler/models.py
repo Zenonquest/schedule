a2ed19cc520fb3f63dec25d016116be0cfcaddc1
Zenon <Zenonquest@gmail.com>
2016-06-02 16:53:13 -0600

    changed get to filter

--- scheduler/models.py
+++ scheduler/models.py
@@ -14,7 +14,8 @@ class Teacher(models.Model):
 	nickname = models.CharField(max_length=200, unique=True)
 
 	def get_availability(self):
-		availability = Availability.objects.get(teacher=self.teacher_id)[:1]
+		# t_id = self.teacher_id
+		availability = Availability.objects.filter(teacher=self.teacher_id)[:1]
 		return availability
 
 	def __unicode__(self):
