56c9515c972524bed22c8e10090e6774d087481e
Zenonquest <Zenonquest@gmail.com>
2016-07-23 22:53:22 -0600

    update dependencies

--- scheduler/skills.py
+++ scheduler/skills.py
@@ -1,8 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from rest_framework import status, viewsets, permissions
 
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseBadRequest
 
 from .models import Skill
 from .serializers import SkillSerializer
